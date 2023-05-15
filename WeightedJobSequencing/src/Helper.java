08197abbc50d3b817647450a1d846e11133e8131
Serag el-dein Amged <71095930+SeragAmged@users.noreply.github.com>
2023-05-15 20:33:57 +0300

    binary search

--- WeightedJobSequencing/src/Helper.java
+++ WeightedJobSequencing/src/Helper.java
@@ -1,6 +1,26 @@
 import java.util.Arrays;
 
 public class Helper {
+    private static int binarySearch(int[][] jobs, int i) {
+        int low = 0;
+        int high = i - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (jobs[mid][1] <= jobs[i][0]) {
+                if (jobs[mid + 1][1] <= jobs[i][0]) {
+                    low = mid + 1;
+                } else {
+                    return mid;
+                }
+            } else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+
     public static int maximumProfit(int[][] jobs) {
         Arrays.sort(jobs, (a, b) -> Integer.compare(a[1], b[1]));
 
@@ -16,9 +36,9 @@ public class Helper {
             if (prevJobIndex != -1) {
                 profit += solutionArray[prevJobIndex];
             }
-            solutionArray[i] = Math.max(solutionArray[i-1], profit);
+            solutionArray[i] = Math.max(solutionArray[i - 1], profit);
         }
 
-        return solutionArray[length-1];
+        return solutionArray[length - 1];
     }
 }
